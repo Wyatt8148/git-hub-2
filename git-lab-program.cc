2c9b94806f03cfd6a4dfdaca973fcb11c5a609f7
Wyatt Abella <wa036418@ohio.edu>
2021-01-27 19:46:55 -0500

    Hello Git code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Wyatt Abella
+ *        Date: 01/27/21
+ * Description: github lab 1
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
